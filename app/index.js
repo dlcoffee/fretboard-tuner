b16458d6c553d89bceaafd90569a62d698b53b00
Daniel Tea <daniel_tea@spe.sony.com>
2017-06-04 00:34:31 -0700

    add different sounds

--- app/index.js
+++ app/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom';
 
 import './app.scss';
 
+import Tables from './Tables';
+
 class String extends React.Component {
   constructor(props) {
     super(props);
 
     this.onClick = this.onClick.bind(this);
+    this.synthesizedSound = this.synthesizedSound.bind(this);
+  }
+
+  synthesizedSound() {
+    let { context } = this.props;
+
+    let sound = Tables.organ;
+    let real = new Float32Array(sound.real.length);
+    let imag = new Float32Array(sound.real.length);
+
+    for (var i = 0; i < sound.real.length; i++) {
+      real[i] = sound.real[i];
+      imag[i] = sound.imag[i];
+    }
+
+    let wave = context.createPeriodicWave(real, imag);
+
+    return wave;
   }
 
   onClick() {
@@ -18,17 +38,19 @@ class String extends React.Component {
     let oscillator = context.createOscillator();
     let gainNode = context.createGain();
 
+
     oscillator.connect(gainNode)
     gainNode.connect(context.destination);
 
     oscillator.type = 'sine';
     oscillator.frequency.value = freq;
+    oscillator.setPeriodicWave(this.synthesizedSound());
     gainNode.gain.setValueAtTime(1, context.currentTime);
 
     oscillator.start(context.currentTime);
 
-    gainNode.gain.exponentialRampToValueAtTime(0.001, context.currentTime + 1);
-    oscillator.stop(context.currentTime + 1);
+    gainNode.gain.exponentialRampToValueAtTime(0.001, context.currentTime + 3);
+    oscillator.stop(context.currentTime + 3);
   }
 
   render() {
